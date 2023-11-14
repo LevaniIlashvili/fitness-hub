818436413c74cf7490e672f3acfbe1f4c34fc11f
LevaniIlashvili <levaniko900@gmail.com>
2023-11-14 17:28:18 +0400

    add horizontal scrolling with navigation arrows for smaller screens

--- src/components/BodyPartsList.tsx
+++ src/components/BodyPartsList.tsx
@@ -1,14 +1,59 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { useAppSelector, useAppDispatch } from "../app/hooks";
 import { selectBodyPart } from "../app/redux/bodyParts/bodyParts";
 import { goToPage } from "../app/redux/pagination/pagination";
+import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
 
 const BodyPartsList = () => {
   const dispatch = useAppDispatch();
   const selectedBodyPart = useAppSelector(
     (state) => state.bodyParts.selectedBodyPart
   );
+
+  // horizontal scrolling
+  const containerRef = useRef<HTMLUListElement | null>(null);
+  const [scrollLeft, setScrollLeft] = useState<number>(0);
+  const [showScrollButtons, setShowScrollButtons] = useState<boolean>(false);
+
+  const scroll = (direction: string): void => {
+    const container = containerRef.current;
+    if (container) {
+      const scrollAmount = 100; // Adjust this value as needed
+      const containerWidth = container.clientWidth;
+      const maxScrollLeft = container.scrollWidth - containerWidth;
+
+      const newScrollLeft =
+        direction === "left"
+          ? Math.max(scrollLeft - scrollAmount, 0)
+          : Math.min(scrollLeft + scrollAmount, maxScrollLeft);
+
+      container.scrollTo({
+        left: newScrollLeft,
+        behavior: "smooth",
+      });
+
+      setScrollLeft(newScrollLeft);
+    }
+  };
+
+  const handleResize = (): void => {
+    const container = containerRef.current;
+    if (container) {
+      const shouldShowButtons = container.scrollWidth > container.clientWidth;
+      setShowScrollButtons(shouldShowButtons);
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const bodyParts = [
     "all",
@@ -26,31 +71,72 @@ const BodyPartsList = () => {
 
   return (
     <Wrapper>
-      {bodyParts.map((bodyPart, index) => {
-        return (
-          <li key={index}>
-            <input
-              type="radio"
-              name="body_parts"
-              id={bodyPart}
-              checked={bodyPart === selectedBodyPart}
-              onChange={() => {
-                dispatch(selectBodyPart(bodyPart));
-                dispatch(goToPage(0));
-              }}
-            />
-            <label htmlFor={bodyPart}>{bodyPart}</label>
-          </li>
-        );
-      })}
+      {showScrollButtons && (
+        <button className="scroll-button" onClick={() => scroll("left")}>
+          <LuChevronLeft />
+        </button>
+      )}
+      <ul ref={containerRef}>
+        {bodyParts.map((bodyPart, index) => {
+          return (
+            <li key={index}>
+              <input
+                type="radio"
+                name="body_parts"
+                id={bodyPart}
+                checked={bodyPart === selectedBodyPart}
+                onChange={() => {
+                  dispatch(selectBodyPart(bodyPart));
+                  dispatch(goToPage(0));
+                }}
+              />
+              <label htmlFor={bodyPart}>{bodyPart}</label>
+            </li>
+          );
+        })}
+      </ul>
+      {showScrollButtons && (
+        <button className="scroll-button" onClick={() => scroll("right")}>
+          <LuChevronRight />
+        </button>
+      )}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.ul`
+const Wrapper = styled.section`
   display: flex;
-  gap: 0.5rem;
-  list-style: none;
+  gap: 0.2rem;
+
+  ul {
+    display: flex;
+    align-items: center;
+    height: 4rem;
+    width: 64vw;
+    gap: 0.5rem;
+    list-style: none;
+    overflow-x: auto;
+    -ms-overflow-style: none; /* Internet Explorer 10+ */
+    scrollbar-width: none; /* Firefox */
+  }
+
+  ul::-webkit-scrollbar {
+    display: none; /* Safari and Chrome */
+  }
+
+  .scroll-button {
+    border: none;
+    font-size: 2rem;
+    cursor: pointer;
+  }
+
+  .scroll-button:hover {
+    background-color: #97979784;
+  }
+
+  li {
+    white-space: nowrap;
+  }
 
   input {
     display: none;
